fb9949adb903d83f032ac5ab76e374424bdf6d55
bowen-anth <bowen.anth@gmail.com>
2023-12-23 15:06:02 -0700

    passing props into Card

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,29 +1,29 @@
 import React from "react"
 
-const Card = ({ data }) => {
+const Card = (props) => {
 
-    console.log(data)
+    console.log(props)
 
     let popular
-    if (props.data.popular === true) {
+    if (props.popular === true) {
         popular = "POPULAR"
     }
     let soldOut
-    if (props.data.available === true) {
+    if (props.available === true) {
         soldOut = "Sold out"
     }
     return (
         <>
             <div className="card-container">
-                {popular && <div className="card--badge">{popular}</div>}
-                <img src="https://csyxkpbavpcrhwqhcpyy.supabase.co/storage/v1/object/public/assets/coffee-challenge/cappuccino.jpg" 
-                alt="" width="200px" height="height" />
-                <p className="coffee-price-p">Cappuccino<span className="price-span">$5.20</span></p>
+                {/* {popular && <div className="card--badge">{popular}</div>} */}
+                <img src={props.image}
+                alt={`a picture of ${props.name}`} width="200px" height="height" />
+                <p className="coffee-price-p">{props.name}<span className="price-span">{props.price}</span></p>
                 <div className="rating-container">
                     <img src="public/Star_fill.svg" alt="a filled star" width="10px" height="auto"/>
-                    <span className="rating-span">4.9</span>
-                    <span className="votes-span">(55 votes)</span>
-                    {soldOut && <span className="sold-out-span">{soldOut}</span>}
+                    <span className="rating-span">{props.rating}</span>
+                    <span className="votes-span">{props.votes ? `(${props.votes})` : "No ratings"}</span>
+                    {/* {soldOut && <span className="sold-out-span">{soldOut}</span>} */}
                 </div>
             </div>
         </>
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header.jsx'
-import Buttons from './components/Buttons.jsx'
-import Card from './components/Card.jsx'
-import data from './data/data.js'
+import Header from '../components/Header.jsx'
+import Buttons from '../components/Buttons.jsx'
+import Card from '../components/Card.jsx'
+import data from '/Users/mac/Scrimba/simple-coffee-listing/data/data.js'
 
 function App() {
-
+  const MAX_ITEMS = 10;
+  const cards = data.slice(0, MAX_ITEMS).map((item) => {
+    return (
+      <Card {...item} />
+    )
+   })
 
   return (
     <>
      <Header />
      <Buttons />
-     <Card {...data} />
+     {cards}
     </>
   )
 }
